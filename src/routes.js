17d15fe4519a55a70ad04d1de3c84d8e4498ea64
Jhonatanmizu <natanjesuss20@gmail.com>
2021-09-23 14:52:07 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
-import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createAppContainer, createSwitchNavigator, createDrawerNavigator } from "react-navigation";
 import login from "./pages/login"
 import createAccount from "./pages/createAccount";
 import accountInfo from "./pages/accountInfo"
+import Home from "./pages/Home"
+import MonthPlans from "./pages/MonthPlans"
+import Relatorio from "./pages/Relatorio";
 const Routes = createAppContainer(
     createSwitchNavigator({
         login,
         createAccount,
-        accountInfo
+        accountInfo,
+        Home,
+        MonthPlans,
+        Relatorio
     })
+    
 )
 export default Routes;
