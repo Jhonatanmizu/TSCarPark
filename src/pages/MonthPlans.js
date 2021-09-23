5a099026bd860e3d904253b53231c269bc5246c4
Jhonatanmizu <natanjesuss20@gmail.com>
2021-09-23 14:45:36 -0300

    added new pages

---
+++ src/pages/MonthPlans.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {View, Image,Text,TextInput,TouchableOpacity,StyleSheet,KeyboardAvoidingView,Platform} from 'react-native'
+export default function MonthPlans(){
+return(
+<View></View>
+);
+}
---
+++ src/pages/Relatorio.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Image, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native'
+export default function Relatorio() {
+    return (
+        <View>
+
+        </View>
+    );
+}
