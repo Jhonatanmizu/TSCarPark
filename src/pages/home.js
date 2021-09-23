cb12974b9e0aabd767fc7a71bf373fac855af551
Jhonatanmizu <natanjesuss20@gmail.com>
2021-09-23 14:53:44 -0300

    installed new packages

--- src/pages/home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {View, Image,Text,TextInput,TouchableOpacity,StyleSheet,KeyboardAvoidingView,Platform} from 'react-native'
-export default function home(){
-return(
-<View>
-
-</View>
-);
-}
---
+++ src/styles/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { StyleSheet } from 'react-native';
+const homeStyle = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+})
+
+export default homeStyle;
